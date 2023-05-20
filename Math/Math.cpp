3a0fe7bc8b5e42ab0e246a70fdc46979965d48db
titanium-22 <titan.kaneda@gmail.com>
2023-05-20 16:52:15 +0900

    edit math.cpp

--- Math/Math.cpp
+++ Math/Math.cpp
@@ -1,16 +1,16 @@
 long long pow(long long a, long long b, long long mod) {
   long long res = 1ll;
   while (b) {
-    if (b & 1) res = res * a % mod;
+    if (b & 1ll) res = res * a % mod;
     a = a * a % mod;
-    b >>= 1;
+    b >>= 1ll;
   }
   return res;
 }
 
 long long factorial(long long x) {
   long long res = 1;
-  for (long long i = 1; i <= x; ++i) {
+  for (long long i = 1ll; i <= x; ++i) {
     res *= i;
   }
   return res;
