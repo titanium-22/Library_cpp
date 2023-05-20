7da0f350990cf0faa4f2405b4b9b239559f72a0b
titanium-22 <titan.kaneda@gmail.com>
2023-05-20 20:01:59 +0900

    edit Math.cpp

--- Math/Math.cpp
+++ Math/Math.cpp
@@ -1,8 +1,18 @@
-long long pow(long long a, long long b, long long mod) {
+long long pow_mod(long long a, long long b, long long mod) {
   long long res = 1ll;
   while (b) {
     if (b & 1ll) res = res * a % mod;
     a = a * a % mod;
+    b >>= 1ll;
+  }
+  return res;
+}
+
+long long pow(long long a, long long b) {
+  long long res = 1ll;
+  while (b) {
+    if (b & 1ll) res *= a;
+    a *= a;
     b >>= 1ll;
   }
   return res;
